fbff9719850828ac17755295a5032da5bde7e4ae
Robert Zych <robert.zych@gmail.com>
2021-09-11 15:54:18 -0700

    removed tweet limit and include tweet count in output topic

--- streams-classifier/src/main/java/com/github/robertzych/classifier/TweetClassifier.java
+++ streams-classifier/src/main/java/com/github/robertzych/classifier/TweetClassifier.java
@@ -97,9 +97,9 @@ public class TweetClassifier {
                 .groupByKey()
                 .aggregate(() -> 0L, (k, v, a) -> a + 1, materialized)
                 .toStream()
-                .filter((k, v) -> v == 2L)  // TODO: use threshold instead of hard-coded value
-                .peek((k, v) -> log.info("ScreenName={}", k))
-                .map((k, v) -> new KeyValue<>(k, k))
+//                .filter((k, v) -> v == 1L)  // TODO: use threshold instead of hard-coded value
+                .peek((k, v) -> log.info("ScreenName={},count={}", k, v))
+                .map((k, v) -> new KeyValue<>(k, String.format("ScreenName=%s,count=%s", k, v)))
                 .to(options.getUsersTopic(), Produced.with(Serdes.String(), Serdes.String()));
 
         return builder;
